1b40dce4c81422cfacac42ff2cfd9300d53fbecf
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-06-14 20:13:22 +1000

    Added descriptions to existing tests.

--- tests/tests.py
+++ tests/tests.py
@@ -34,18 +34,25 @@ class TestSassyMedia(unittest.TestCase):
             print self.SM.contents
             raise e
 
+#-------------------------------------------------------------------------------
+
     def test_is_class(self):
         self.assertIsInstance(self.SM, sassymedia.SassyMedia,
             "Instance does not match class.")
 
     def test_no_media_query(self):
-        self._run_match_test("no_media_query")
+        self._run_match_test("no_media_query",
+            "Input with no media query did not product expected output")
 
     def test_single_query_at_end(self):
-        self._run_match_test("single_query_at_end")
+        self._run_match_test("single_query_at_end",
+            "Input with a single media query at the end did not product expected output")
 
     def test_single_query_at_beginning(self):
-        self._run_match_test("single_query_at_beginning")
+        self._run_match_test("single_query_at_beginning",
+            "Input with a single media query at the beginning did not product expected output")
+
+#-------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
